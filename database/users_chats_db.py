6e12cca1f8ff9444ebddce05f46a8794daf95ea1
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2024-10-08 20:11:35 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -11,7 +11,8 @@ class Database:
         self.grp = self.db.group
         self.req_one = self.db.reqone
         self.req_two = self.db.reqtwo
-        self.fsub = self.db.fsub
+        self.fsub1 = self.db.fsub1
+        self.fsub2 = self.db.fsub2
         self.chat_col = self.db.chatcol
         self.chat_col2 = self.db.chatcol2
 
@@ -217,13 +218,23 @@ class Database:
         await self.chat_col2.delete_one({"chat_id": chat_id})
         await self.req_two.delete_many({})
 
-    async def get_fsub_mode(self):
-        return await self.fsub.find_one({})
+    async def get_fsub_mode1(self):
+        return await self.fsub1.find_one({})
 
-    async def add_fsub_mode(self, mode):
+    async def add_fsub_mode1(self, mode):
         try:
-            await self.fsub.delete_many({})
-            await self.fsub.insert_one({"mode": mode})
+            await self.fsub1.delete_many({})
+            await self.fsub1.insert_one({"mode": mode})
+        except:
+            pass
+
+    async def get_fsub_mode2(self):
+        return await self.fsub2.find_one({})
+
+    async def add_fsub_mode2(self, mode):
+        try:
+            await self.fsub2.delete_many({})
+            await self.fsub2.insert_one({"mode": mode})
         except:
             pass
         
