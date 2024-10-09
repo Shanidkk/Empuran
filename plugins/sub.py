c549878e65fb1e5b761beaa367ab2216d73a0c88
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2024-10-09 20:31:05 +0530

    Update sub.py

--- plugins/sub.py
+++ plugins/sub.py
@@ -1,16 +1,17 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import ChatJoinRequest, Message
-from info import REQ_CHANNEL1, REQ_CHANNEL2, ADMINS
+from info import ADMINS
 from database.users_chats_db import db
+from utils import temp
 
-@Client.on_chat_join_request(filters.chat(REQ_CHANNEL1) | filters.chat(REQ_CHANNEL2))
+@Client.on_chat_join_request(filters.chat(temp.REQ_CHANNEL1) | filters.chat(temp.REQ_CHANNEL2))
 async def join_reqs(b, join_req: ChatJoinRequest):
     user_id = join_req.from_user.id
     try:
-        if join_req.chat.id == REQ_CHANNEL1:
+        if join_req.chat.id == temp.REQ_CHANNEL1:
             if join_req.invite_link.creator.id == b.me.id:
                 await db.add_req_one(user_id)
-        if join_req.chat.id == REQ_CHANNEL2:
+        if join_req.chat.id == temp.REQ_CHANNEL2:
             if join_req.invite_link.creator.id == b.me.id:
                 await db.add_req_two(user_id)
     except Exception as e:
