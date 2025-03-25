8915ac002a3f8f34cd2c299a7c658309aa778865
Muhammedshifin <muhammedshifink4@gmail.com>
2025-03-25 22:10:52 +0530

    Update fsub_manager.py

--- plugins/fsub_manager.py
+++ plugins/fsub_manager.py
@@ -38,7 +38,8 @@ async def set_fsub_chat1(bot: Client, message: Message):
     await message.reply_text(text, quote=True, parse_mode=enums.ParseMode.HTML)
 
     invite_link = await create_invite(bot, chat_id, temp.REQ_FSUB_MODE1)
-    await db.add_fsub_chat1(chat_id, invite_link)
+    mode = await db.get_fsub_mode1()
+    await db.add_fsub_chat1(chat_id, invite_link, mode)
 
     bot.req_link1 = invite_link
     temp.REQ_CHANNEL1 = chat_id
@@ -97,7 +98,8 @@ async def set_fsub_chat2(bot: Client, message: Message):
     await message.reply_text(text, quote=True, parse_mode=enums.ParseMode.HTML)
 
     invite_link = await create_invite(bot, chat_id, temp.REQ_FSUB_MODE2)
-    await db.add_fsub_chat2(chat_id, invite_link)
+    mode = await db.get_fsub_mode2()
+    await db.add_fsub_chat2(chat_id, invite_link, mode)
 
     bot.req_link2 = invite_link
     temp.REQ_CHANNEL2 = chat_id
